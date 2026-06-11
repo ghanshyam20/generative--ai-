aac99a44173e50bcc40652c2ab16d9f20ea46409
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2026-06-12 00:44:00 +0300

    inital commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import tiktoken 
+
+enc=tiktoken.encoding_for_model("gpt-4o")
+
+
+text="Hello nokia i am coming"
+
+# tokens=enc.encode(text)
+
+
+#tokens [13225, 18727, 535, 575, 939, 7245]
+# print("tokens", tokens)
+
+
+decoded=enc.decode([13225, 18727, 535, 575, 939, 7245])
+
+print("decoded", decoded)
+
